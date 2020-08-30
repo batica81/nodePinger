37b9d7a2068fcee497d24d2f65cdc96c6d73c657
Vojislav Ristivojevic <batica@gmail.com>
2020-08-30 17:01:57 +0200

    update script changed

--- php/client_update.sh
+++ php/client_update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Crontab line:
+# */10 * * * * /home/user/client_update.sh
+
 curl -user test:password -X POST -H "Content-Type: application/x-www-form-urlencoded" -d
 "client_ip=NAMEORIP" https://.duckdns.org/keepalive/index.php
